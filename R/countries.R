91d286ad1f56437bb76e306f99e047bc021f59e2
Adam Howes <adamhowes@outlook.com>
2022-02-03 14:34:59 +0000

    Add country shortcuts

---
+++ R/countries.R
@@ -0,0 +1,23 @@
+priority_iso3 <- function() {
+  c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+}
+
+analysis_level <- function() {
+  c(
+    "BWA" = 2, "CMR" = 2, "KEN" = 2,
+    "LSO" = 1, "MOZ" = 2, "MWI" = 5,
+    "NAM" = 2, "SWZ" = 1, "TZA" = 3,
+    "UGA" = 3, "ZAF" = 2, "ZMB" = 2,
+    "ZWE" = 2
+  )
+}
+
+admin1_level <- function() {
+  c(
+    "BWA" = 1, "CMR" = 1, "KEN" = 1,
+    "LSO" = 1, "MOZ" = 1, "MWI" = 1,
+    "NAM" = 2, "SWZ" = 1, "TZA" = 2,
+    "UGA" = 1, "ZAF" = 1, "ZMB" = 1,
+    "ZWE" = 1
+  )
+}
