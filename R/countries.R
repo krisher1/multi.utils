7d2685fddc2032462556f0927c5a3a3c753116cb
Katie Risher <krisher@ic.ac.uk>
2022-12-21 11:15:26 -0600

    Expand Countries
    Include 30 countries to be in update

--- R/countries.R
+++ R/countries.R
@@ -1,5 +1,7 @@
 priority_iso3 <- function() {
-  c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE")
+  c("BWA", "CMR", "KEN", "LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZAF", "ZMB", "ZWE",
+    "AGO", "BDI", "COD", "GAB", "RWA", "ETH", "HTI", "TCD", "CIV", "GHA", "GIN", "LBR", "MLI",
+    "NER", "SLE", "TGO", "BFA")
 }
 
 analysis_level <- function() {
@@ -8,7 +10,12 @@ analysis_level <- function() {
     "LSO" = 1, "MOZ" = 3, "MWI" = 5,
     "NAM" = 2, "SWZ" = 1, "TZA" = 4,
     "UGA" = 3, "ZAF" = 2, "ZMB" = 2,
-    "ZWE" = 2
+    "ZWE" = 2, "AGO" = 1, "BDI" = 2,
+    "BFA" = 2, "CIV" = 2, "COD" = 3,
+    "ETH" = 2, "GAB" = 1, "GHA" = 2,
+    "GIN" = 2, "HTI" = 2, "LBR" = 1,
+    "MLI" = 1, "NER" = 1, "RWA" = 2,
+    "SLE" = 2, "TCD" = 1, "TGO" = 2
   )
 }
 
@@ -18,6 +25,11 @@ admin1_level <- function() {
     "LSO" = 1, "MOZ" = 1, "MWI" = 1,
     "NAM" = 2, "SWZ" = 1, "TZA" = 2,
     "UGA" = 1, "ZAF" = 1, "ZMB" = 1,
-    "ZWE" = 1
+    "ZWE" = 1, "AGO" = 1, "BDI" = 2,
+    "BFA" = 2, "CIV" = 1, "COD" = 1,
+    "ETH" = 1, "GAB" = 1, "GHA" = 1,
+    "GIN" = 1, "HTI" = 1, "LBR" = 1,
+    "MLI" = 1, "NER" = 1, "RWA" = 1,
+    "SLE" = 1, "TCD" = 1, "TGO" = 1
   )
 }
